d607b92e1d423df642a4bcc8a071a929174a86ad
BalaM14 <balamurugan141196@gmail.com>
2026-06-01 11:01:51 +0530

    Updated to get the api key in run time

--- config.py
+++ config.py
@@ -4,11 +4,22 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+try:
+    import streamlit as st
+
+    GROQ_API_KEY = st.secrets.get("GROQ_API_KEY")
+
+except Exception:
+    GROQ_API_KEY = None
 
 if not GROQ_API_KEY:
-    raise ValueError("GROQ_API_KEY not found. Add it in .env file.")
+    GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
-MODEL_NAME = "llama-3.3-70b-versatile"
+if not GROQ_API_KEY:
+    raise ValueError(
+        "GROQ_API_KEY not found in Streamlit Secrets or environment variables."
+    )
+
+MODEL_NAME = "llama-3.1-8b-instant"
 
 DB_PATH = Path("database/sales_poc.db")
